cf5db634e9f60f1fcd3afe42c8ea094b24de2a86
SANDESHAHAHA <sandeshguragain516@gmail.com>
2024-09-29 21:32:02 +0545

    stream error

---
+++ filehandling/streamerror.cpp
@@ -0,0 +1,21 @@
+//checking stream  buffers;
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    while(true){
+        cout<<"Enter a number: "<<endl;
+        cin>>x;
+        cin.ignore(10,'\n');
+        if(cin.good())
+        {
+            break;
+        }
+        cin.clear();
+        cout<<"Incorrect output"<<endl;
+        cin.ignore(10,'\n');
+    }
+    cout<<"Integer is : "<<x<<endl;
+    return 0;
+}
